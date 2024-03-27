c528e340df5ccfa5084cd4a4ca42b1c3322d07ee
plucafo <plucaf87@gmail.com>
2024-03-27 15:02:28 -0700

    set up additional middlware and added descriptive comments. created connection.js file and a schema.sql file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// dependencies
 const express = require("express");
 const path = require("path");
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
 // set up the express app
@@ -8,16 +9,24 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // set handlebars as the templating engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// middleware to parse json post/put requests so they are available in the req.body
+app.use(express.json());
+
+// middleware to parse incoming request bodies in url encoded format commonly sent by HTML forms when data is submitted
+app.use(express.urlencoded({ extended: false }));
 
 // middleware to look for static files in the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+// route to landing page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
+// starts the server
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
