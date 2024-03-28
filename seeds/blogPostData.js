86aa6dd5fdd8b6f23a9262ba4001033c8484a0ce
plucafo <plucaf87@gmail.com>
2024-03-28 15:32:07 -0700

    created working seed file to seed the BlogPost table with data

---
+++ seeds/blogPostData.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const BlogPost = require("../models/BlogPost");
+
+const blogPostData = [
+  {
+    title: "Why MVC is so important",
+    content:
+      "MVC allows developers to maintain a true separation of concerns, deviding their code between the Model layer for data, the View layer for design, and the Controller layer for application logic.",
+    author: "Milhouse",
+  },
+  {
+    title: "Authentication vs. Authorization",
+    content:
+      "There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed to access the system.",
+    author: "Ralph",
+  },
+];
+
+const seedBlogPost = async () => {
+    await sequelize.sync({ force: true });
+    await BlogPost.bulkCreate(blogPostData);
+    process.exit(0)
+}
+
+seedBlogPost();
