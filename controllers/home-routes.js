40a78fd021efbda0f354d2d9c58a29b37e942712
plucafo <plucaf87@gmail.com>
2024-03-29 10:46:28 -0700

    set up route to /login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,4 +16,8 @@ router.get("/dashboard", async (req, res) => {
   res.render("dashboard");
 });
 
+router.get("/login", async (req, res) => {
+  res.render("login");
+});
+
 module.exports = router;
