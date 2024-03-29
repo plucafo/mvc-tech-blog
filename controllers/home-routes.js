81d73227c82fa8c449981274a8393fd6e47c2b5a
plucafo <plucaf87@gmail.com>
2024-03-29 10:43:57 -0700

    pulled in BlogPost data to display on my home view. Created a route for /dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,12 +2,18 @@ const router = require("express").Router();
 const BlogPost = require("../models/BlogPost");
 
 router.get("/", async (req, res) => {
-    res.render("all");
-  });
+  const postData = await BlogPost.findAll();
+  const posts = postData.map((data) => data.get({ plain: true }));
+  res.render("home", { posts });
+});
 
-router.get("/post/:id"), async (req, res) => {
+router.get("/post/:id"),
+  async (req, res) => {
     res.render("post");
-}
+  };
+
+router.get("/dashboard", async (req, res) => {
+  res.render("dashboard");
+});
 
 module.exports = router;
-
