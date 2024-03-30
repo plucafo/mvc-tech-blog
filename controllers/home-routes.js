e5ce499ae1e1ba334262d83c13d46fa68443245b
plucafo <plucaf87@gmail.com>
2024-03-30 11:26:54 -0700

    /post/:id route is working when the user clicks on a post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const BlogPost = require("../models/BlogPost");
 
+// home '/' route to show all blog posts
 router.get("/", async (req, res) => {
   const logoText = 'The Tech Blog';
   const postData = await BlogPost.findAll();
@@ -8,16 +9,28 @@ router.get("/", async (req, res) => {
   res.render("home", { posts, logoText });
 });
 
+// route to show a single post
 router.get("/post/:id"),
   async (req, res) => {
-    res.render("post");
+    const logoText = `Post #${req.params.id}`;
+    const postData = await BlogPost.findByPk(req.params.id);
+    const posts = postData.get({ plain: true });
+    res.render("post", { posts, logoText });
   };
+  router.get("/post/:id", async (req, res) => {
+    const logoText = `Post #${req.params.id}`;
+    const postData = await BlogPost.findByPk(req.params.id);
+    const post = postData.get({ plain: true });
+    res.render("post", { post, logoText });
+  });
 
+// dashboard route
 router.get("/dashboard", async (req, res) => {
   const logoText = 'Dashboard';
   res.render("dashboard", { logoText });
 });
 
+// login route
 router.get("/login", async (req, res) => {
   const logoText = 'Login';
   res.render("login", { logoText });
